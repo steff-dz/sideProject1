d4d5e8b483a3b09fd45d76539369a4a249f9b0ae
Steff <stefaniemarie@gmail.com>
2020-11-22 22:30:45 +0100

    added box animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const arrows = document.querySelectorAll('.downArrow');
 const answers = document.querySelectorAll('p');
 const questions = document.querySelectorAll('h2');
+const box = document.querySelector('#boxImg');
 
 for (let i = 0; i < questions.length; i++) {
 	questions.item(i).addEventListener('click', function() {
@@ -10,6 +11,7 @@ for (let i = 0; i < questions.length; i++) {
 					questions.item(j).style.fontWeight == '400';
 					answers.item(j).style.visibility = 'hidden';
 					arrows.item(j).classList.remove('animateArrow');
+					box.classList.remove('moveBox');
 				}
 			}
 			questions.item(i).style.fontWeight = '700';
@@ -17,11 +19,13 @@ for (let i = 0; i < questions.length; i++) {
 			answers.item(i).style.top = '0px';
 			answers.item(i).style.paddingBottom = '10px';
 			arrows.item(i).classList.add('animateArrow');
+			box.classList.add('moveBox');
 		} else {
 			questions.item(i).style.fontWeight = '400';
 			answers.item(i).style.visibility = 'hidden';
 			answers.item(i).style.paddingBottom = '0';
 			arrows.item(i).classList.remove('animateArrow');
+			box.classList.remove('moveBox');
 		}
 	});
 }
