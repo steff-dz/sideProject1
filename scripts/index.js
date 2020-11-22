8dd381705010dbfceee02392598ba86795b1cc21
Steff <stefaniemarie@gmail.com>
2020-11-22 21:15:09 +0100

    functionality going

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const arrows = document.querySelectorAll('.downArrow');
+const answers = document.querySelectorAll('p');
+const questions = document.querySelectorAll('h2');
+
+for (let i = 0; i < questions.length; i++) {
+	questions.item(i).addEventListener('click', function() {
+		if (answers.item(i).style.visibility === 'hidden' || answers.item(i).style.visibility === '') {
+			for (let j = 0; j < questions.length; j++) {
+				if (answers.item(j).style.visibility === 'visible') {
+					questions.item(j).style.fontWeight == '400';
+					answers.item(j).style.visibility = 'hidden';
+					arrows.item(j).classList.remove('animateArrow');
+				}
+			}
+			questions.item(i).style.fontWeight = '700';
+			answers.item(i).style.visibility = 'visible';
+			answers.item(i).style.top = '0px';
+			answers.item(i).style.paddingBottom = '10px';
+			arrows.item(i).classList.add('animateArrow');
+		} else {
+			questions.item(i).style.fontWeight = '400';
+			answers.item(i).style.visibility = 'hidden';
+			answers.item(i).style.paddingBottom = '0';
+			arrows.item(i).classList.remove('animateArrow');
+		}
+	});
+}
